b18d0b6763f7331cd6e341f50efca0a157c39051
Vendula Vankova <vendula.van@gmail.com>
2022-04-30 16:46:35 +0200

    Final HTML Structure + JS dataset

---
+++ kviz.js
@@ -0,0 +1,36 @@
+const otazky = [{
+        otazka: 'Jak se jmenují prasynovci strýčka Skrblíka?',
+        obrazek: 'kaceri.jpg',
+        alt: 'Kačeří příběhy',
+        odpovedi: [
+            'Kulík, Dulík a Bulík',
+            'Kulík, Dulík a Bubík',
+            'Kubík, Dulík a Bulík'
+        ],
+        spravna: 1
+    },
+    {
+        otazka: 'Kdo byl hlavním protivníkem Strážců vesmíru v kultovním seriálu z roku 1993?',
+        obrazek: 'strazcivesmiru.jpg',
+        alt: 'Strážci vesmíru',
+        odpovedi: [
+            'Rita Repulsa',
+            'Zordon',
+            'Alpha 5'
+        ],
+        spravna: 0
+
+    },
+    {
+        otazka: 'Jak se jmenuje modelínový hrdina švýcarsko-britského seriálu z roku 1986?',
+        obrazek: 'tucnak.jpg',
+        alt: 'Tučňáci',
+        odpovedi: [
+            'Pingui',
+            'Beze jména',
+            'Pingu'
+        ],
+        spravna: 2
+
+    }
+];
