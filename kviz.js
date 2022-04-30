c5249670f83e034c0f0427b1bee0d1e1253c93bb
Vendula Vankova <vendula.van@gmail.com>
2022-04-30 18:38:28 +0200

    List Items + Event Handler klikNaOdpoved

--- kviz.js
+++ kviz.js
@@ -44,3 +44,32 @@ const moznosti = document.getElementById('moznosti');
 
 let aktualniOtazka = 0;
 let meOdpovedi = [];
+
+// zobrazení otázek
+
+zobrazOtazku();
+
+function zobrazOtazku() {
+    //upravení pořadí, textu a obrázku otázky
+    poradi.textContent = 'Otázka ' + (aktualniOtazka + 1) + ' / ' + otazky.length;
+    otazka.textContent = otazky[aktualniOtazka].otazka;
+    obrazek.src = 'obrazky/' + otazky[aktualniOtazka].obrazek;
+
+    //upravení odpovědi (přes proměnnou)
+    let odpovedi = otazky[aktualniOtazka].odpovedi;
+
+    let seznam = document.createElement('ul');
+    seznam.id = 'odpovedi';
+
+    //vytvoření list items pro všechny odpovědi a event handler onclick
+    for (let i = 0; i < odpovedi.length; i++) {
+        let polozka = document.createElement('li');
+        polozka.dataset.odpoved = i;
+        polozka.textContent = odpovedi[i];
+        polozka.onclick = klikNaOdpoved;
+        seznam.appendChild(polozka);
+    }
+
+    document.getElementById('odpovedi').remove();
+    moznosti.appendChild(seznam);
+}
